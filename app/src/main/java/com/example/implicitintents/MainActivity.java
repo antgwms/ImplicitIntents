4ba780c7558cad4c08cac35c8d4578ddf76f240e
antgwms <antgwms@outlook.com>
2021-06-08 23:30:06 +0100

    Update MainActivity.java
    Implement methods for two buttons; open a website & open a location

--- app/src/main/java/com/example/implicitintents/MainActivity.java
+++ app/src/main/java/com/example/implicitintents/MainActivity.java
@@ -2,13 +2,54 @@ package com.example.implicitintents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mWebsiteEditText;
+    private EditText mLocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mWebsiteEditText = findViewById(R.id.website_edittext);
+        mLocationEditText = findViewById(R.id.location_edittext);
     }
-}
+
+    public void openWebsite(View view) {
+        //Get the URL as text
+        String url = mWebsiteEditText.getText().toString();
+        //Parse the URI and create an intent
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+
+        //Find an Activity to pass the Intent and start that Activity
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+            else {
+                Log.d("ImplicitIntents", "Can't handle this!");
+            }
+        }
+
+    }
+
+    public void openLocation(View view) {
+        //Get the location as Text
+        String loc = mLocationEditText.getText().toString();
+        // Parse the string into a Uri object with a geo search query
+        Uri addressUri = Uri.parse("geo:0,0?q=" + loc);
+        //create a new intent with action view
+        Intent intent = new Intent(Intent.ACTION_VIEW, addressUri);
+
+        //find an activity to handle the intent and start that activity
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+        else (Log.d("ImplicirIntents", "Can't handle this intent!");
+        }
+    }
